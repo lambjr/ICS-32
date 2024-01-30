fd1ea09a26199d173cb4c3daabbba2e29884f98e
lambjr <156375181+lambjr@users.noreply.github.com>
2024-01-29 21:36:48 -0800

    Update WP1.py
    created a function that loads the input file and output file and gracefully handles exceptions if the likely two occur. It will also write the reversed data to the output file using the previously defined reverse cypher function

--- WP1.py
+++ WP1.py
@@ -2,3 +2,20 @@ import sys
 
 def reverse_cipher(input_text):
   return input_text[::-1]
+
+def load_file(input_filename,output_filename):
+  try:
+        # Read the original file
+        with open(input_filename, 'r') as file:
+            original_data = file.read()
+
+        reversed_data = reverse_cipher(original_data)
+        with open(output_filename, 'w') as file:
+            file.write(reversed_data)
+        print(f"File '{output_filename}' created successfully with reversed content.")
+  except FileNotFounError:
+        print(f'Error: File {input_filename} not found')
+  except Exception as e:
+        print(f'Error: {e}')
+  
+
